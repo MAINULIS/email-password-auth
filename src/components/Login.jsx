d34181d6a8901aa5ebd7ff9df9bb50f2214f7fe9
Mainul Islam <mainulislam6168@gmail.com>
2023-09-09 09:38:15 +0600

    email verification added to register form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -31,27 +31,29 @@ const Login = () => {
             return;
         }
 
-        signInWithEmailAndPassword(app, email, password)
+        signInWithEmailAndPassword(auth, email, password)
         .then(result =>{
             const loggedUser = result.user;
+            console.log(loggedUser)
             setSuccess('User logged In successfully')
             setError('')
         })
         .catch(error =>{
             setError(error.message)
+            console.log(error.message)
         })
     }
     return (
         <div className='my-container  text-center mt-4 '>
             <h3 className='text-3xl tracking-wide font-bold mb-5 text-zinc-700'>Please Login</h3>
             <form onSubmit={handleLogin} className='p-4'>
-               <input className='border border-gray-500 px-2 ' type="email" name="email" id=" email" placeholder='Your Email' required />
+               <input className='border border-gray-500 px-2 w-96' type="email" name="email" id=" email" placeholder='Your Email' required />
                <br />
-               <input className='border border-gray-500 px-2 mt-4' type="password" name="password" id="password" placeholder='Your Password' required />
+               <input className='border border-gray-500 px-2 mt-4 w-96' type="password" name="password" id="password" placeholder='Your Password' required />
                <br />
                <p className='text-red-500'>{error}</p>
-               <p className='text-blue-600'></p>
-               <input className='border rounded font-medium bg-lime-600 hover:bg-lime-500 hover:text-white mt-5 border-gray-500 px-4 py-1' type="submit" value="Register" />
+               <p className='text-green-600'>{success}</p>
+               <input className='border rounded font-medium bg-lime-600 hover:bg-lime-500 hover:text-white mt-5 border-gray-500 px-4 py-1' type="submit" value="Login" />
                <p><small>New to this website? please <Link className='text-blue-600 underline' to="/register">Register</Link></small></p>
             </form>
         </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
+import {createUserWithEmailAndPassword, getAuth, sendEmailVerification} from 'firebase/auth'
 import app from '../firebase/firebase.config';
 import { Link } from 'react-router-dom';
 
@@ -40,10 +40,20 @@ const Register = () => {
             setError('')
             event.target.reset();
             setSuccess('user has been successfully sign up')
+            sendVerificationEmail(result.user);
         })
         .catch(error =>{
             console.error(error.message);
             setError(error.message)
+        })
+    }
+     
+    // email verification
+    const sendVerificationEmail = (user) =>{
+        sendEmailVerification(user)
+        .then(result =>{
+            console.log(result)
+            alert('Please check your email box and verify the email')
         })
     }
 
@@ -56,18 +66,18 @@ const Register = () => {
         // console.log(event.target.value)
     }
     return (
-        <div className='my-container  text-center mt-4 '>
+        <div className=' my-container text-center mt-4 '>
             <h3 className='text-3xl tracking-wide font-bold mb-5 text-zinc-800'>Please Register</h3>
 
-            <form onSubmit={handleSubmit} className='p-4'>
-               <input onChange={handleEmailChange} className='border border-gray-500 px-2 ' type="email" name="email" id=" email" placeholder='Your Email' required />
+            <form onSubmit={handleSubmit} className='p-4 '>
+               <input onChange={handleEmailChange} className='border border-gray-500 px-2 w-96' type="email" name="email" id=" email" placeholder='Your Email' required />
                <br />
-               <input onBlur={handlePasswordBlur} className='border border-gray-500 px-2 mt-4' type="password" name="password" id="password" placeholder='Your Password' required />
+               <input onBlur={handlePasswordBlur} className='border border-gray-500 px-2 mt-4 w-96' type="password" name="password" id="password" placeholder='Your Password' required />
                <br />
                <p className='text-red-500'>{error}</p>
-               <p className='text-blue-600'>{success}</p>
+               <p className='text-green-600'>{success}</p>
                <input className='border rounded font-medium bg-fuchsia-400 hover:bg-fuchsia-500 mt-5 border-gray-500 px-4 py-1' type="submit" value="Register" />
-               <p><small>New to this website? please <Link className='text-blue-600 underline' to="/login">Login</Link></small></p>
+               <p><small>Have an account? please <Link className='text-blue-600 underline' to="/login">Login</Link></small></p>
             </form>
         </div>
     );
